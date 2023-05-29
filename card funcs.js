4db28741f77b9f58ec2f8c4ebf7aa5acdbedf896
HamzahWareh <hamzahwareh@gmail.com>
2023-05-29 18:47:44 +0300

    Redeem cashback

---
+++ card funcs.js
@@ -0,0 +1,20 @@
+
+  function changeText() {
+    var element = document.getElementById("myElement");
+    var element2 = document.getElementById("myElement2");
+    var element3 = document.getElementById("myElement3");
+    var element4 = document.getElementById("myElement4");
+    element.innerHTML = "$0.0";
+    element2.innerHTML = "Cashback Deposited Into Your Account!"
+    element.innerHTML = "$0.0";
+    element2.innerHTML = "Cashback Deposited Into Your Account!"
+    
+  }
+
+  function changeText2(){
+    var element3 = document.getElementById("myElement3");
+    var element4 = document.getElementById("myElement4");
+    element3.innerHTML = "$0.0";
+    element4.innerHTML = "Cashback Deposited Into Your Account!"
+
+  }
